6f92e73653541a1b89c063e60e2d940aa7987853
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-05-29 19:32:02 -0300

    Propostas do que eu irei fazer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Wellcome from './components/Wellcome/Wellcome'
 import './style/global.css'
 
 function App() {
 
   return (
     <>
+    <Wellcome />
       
     </>
   )
---
+++ src/components/Wellcome/Wellcome.tsx
@@ -0,0 +1,16 @@
+import './wellcome.css'
+
+const Wellcome = () => {
+  return (
+    <>
+    <header>
+
+
+
+{/* pretendo colocar o video aqui e colocar o parallax */}
+    </header>
+    </>
+  )
+}
+
+export default Wellcome
