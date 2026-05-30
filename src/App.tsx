238fc822e6f441b82d4bf65b983072ea7db9f1f2
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-05-30 02:04:39 -0300

    Fazendo a mudança de background

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
 import Wellcome from './components/Wellcome/Wellcome'
 import './style/global.css'
 import introVideo from './assets/video/background_pixel.mp4'
+import secondeIntroVideo from './assets/video/pixel_two.mp4'
 import { ArrowBigDown } from 'lucide-react';
+import { ArrowBigLeftDash } from 'lucide-react';
+import { ArrowBigRightDash } from 'lucide-react';
+import { useState } from 'react';
 function App() {
+
+
+  const videoBackground = {
+    pixelOne: introVideo,
+    pixelTwo: secondeIntroVideo
+  }
+
+  const [backgroundVideo, setBackgroundVideo] = useState<any>(videoBackground.pixelOne)
+
+  const prevBackground = () => setBackgroundVideo(videoBackground.pixelOne)
+  const nextBackground = () => setBackgroundVideo(videoBackground.pixelTwo)
+
+  
+
 
   return (
     <>
     <Wellcome />
     <section id='intro-video'>
       <video autoPlay muted loop playsInline id='bg-video'>
-        <source type='video/mp4' src={introVideo}/>
+        <source type='video/mp4' src={backgroundVideo}/>
       </video>
       <div id='area_arrow'>
-        <ArrowBigDown className='arrows'  size={50} color='white'/>
-        <ArrowBigDown className='arrows'  size={50} color='white'/>
-        <ArrowBigDown className='arrows'  size={50} color='white'/>
+        <ArrowBigLeftDash onClick={prevBackground} className='change-bg' size={50} color='white'/>
+        <ArrowBigDown className='arrows' size={50} color='white'/>
+        <ArrowBigRightDash onClick={nextBackground} className='change-bg' size={50} color='white'/>
       </div>
 
       <div className='scroller_indicator'>
-        {/* scroll to bottom */}
-        <h1 id='scroll_wellcome'>Scroll to bottom</h1>
+        <h1 id='scroll_wellcome'>Scroll Down</h1>
       </div>
     </section>
     </>
