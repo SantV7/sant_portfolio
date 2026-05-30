fe28ed4e312b4796a2aa35f28a3bbbdb6a6a505f
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-05-30 16:39:10 -0300

    Mexendo no links e bg

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,24 @@ import { ArrowBigRightDash } from 'lucide-react';
 import { useState } from 'react';
 
 
+
 function App() {
 
-  const [backgroundVideo, setBackgroundVideo] = useState<string>(introVideo)
+  const video = [introVideo, secondIntroVideo, thirdIntro]
+  const [indexVideo, setIndexVideo] = useState<number>(0)
 
-  const prevBackground = () => setBackgroundVideo(introVideo)
-  const nextBackground = () => setBackgroundVideo(secondIntroVideo)
+  const prevBackground = () => setIndexVideo((prevIndex) => (prevIndex - 1))
+  const nextBackground = () => setIndexVideo((prevIndex) => (prevIndex + 1))
 
   
   //  tenho que colocar o terceiro video
-  
-
 
   return (
     <>
     <Wellcome />
     <section id='intro-video'>
-      <video key={backgroundVideo} autoPlay muted loop playsInline id='bg-video'>
-        <source type='video/mp4' src={backgroundVideo}/>
+      <video key={indexVideo} autoPlay muted loop playsInline id='bg-video'>
+        <source type='video/mp4' src={video[indexVideo]}/>
       </video>
       <div id='area_arrow'>
         <ArrowBigLeftDash onClick={prevBackground} className='change-bg' size={50} color='white'/>
