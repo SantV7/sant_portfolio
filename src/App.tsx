d665beb27848dced9637ea1a982c7563a93af1df
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-05-29 23:57:52 -0300

    More configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import Wellcome from './components/Wellcome/Wellcome'
 import './style/global.css'
-
+import introVideo from './assets/video/background_pixel.mp4'
 function App() {
 
   return (
     <>
     <Wellcome />
-      
+    <section id='intro-video'>
+      <video autoPlay muted loop playsInline id='bg-video'>
+        <source type='video/mp4' src={introVideo}/>
+      </video>
+    </section>
     </>
   )
 }
-
 export default App
--- src/components/Wellcome/Wellcome.tsx
+++ src/components/Wellcome/Wellcome.tsx
@@ -1,16 +1,14 @@
+import { useState } from 'react'
 import './wellcome.css'
-import introVideo from '../../assets/video/background_pixel.mp4'
-import { useEffect, useState } from 'react'
+import { Ellipsis } from 'lucide-react';
 
 const Wellcome = () => {
 
-    const [mobileView, setMobileView] = useState<number>(window.innerWidth)
+    const [showMenu, setShowMenu] = useState<boolean>(true)
+    const handleMenu = () => setShowMenu(!showMenu)
 
-    useEffect(() => {
-        const handleResize = () => setMobileView(window.innerWidth)
-        window.addEventListener('resize', handleResize)
-        return () => removeEventListener('resize', handleResize)
-    }, [])
+
+
 
   return (
     <>
@@ -19,16 +17,15 @@ const Wellcome = () => {
             <h1 className='nick-name'>Sant v7</h1>
         </div>
 
-        <nav id='nav-global' className={` nav-base ${mobileView > 768 ? 'navbar-desktop-style' : 'navbar-mobile-style'}`}>
-            <ul>
+        <Ellipsis id='menu_btn' color='white' onClick={handleMenu} size={40} />
+        {showMenu ? <nav id='main_navbar'>
+            <ul id='ul-list'>
                 <li><a href="#">Inicio</a></li>
                 <li><a href="#">Skills</a></li>
                 <li><a href="#">Projetos</a></li>
+                <a href='#' className='contact_btn'>Contato</a>
             </ul>
-
-            <button className='contact_btn'>Contato</button>
-        </nav>
-
+        </nav> : ''}
     </header>
     </>
   )
