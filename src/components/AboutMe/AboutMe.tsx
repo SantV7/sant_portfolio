c63796bbbfb3c6ccc982997ecf6b5271aeaffbc3
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-06-04 03:33:56 -0300

    Criando uma parte do about me

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useRef } from 'react'
 import { gsap } from 'gsap'
 import oldPc from '../../assets/img/pcimg.png'
-import  newPc from '../../../src/assets/video/new_pc.mp4'
-import  robot from '../../../src/assets/video/bg_about_me.mp4'
+import newPc from '../../../src/assets/video/new_pc.mp4'
+import teclado_pixel from '../../assets/img/teclado.png'
+
 import styles from './AboutMe.module.css'
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
@@ -13,8 +14,8 @@ const AboutMe = () => {
   const WhoRef = useRef<HTMLHeadingElement>(null)
   const sectionRef = useRef<HTMLElement>(null)
 
-   const softwareEngineerRef = useRef<HTMLHeadingElement>(null)
-  const developerRef = useRef<HTMLHeadingElement>(null)
+  //  const softwareEngineerRef = useRef<HTMLHeadingElement>(null)
+  // const developerRef = useRef<HTMLHeadingElement>(null)
 
   useEffect(() => {
     gsap.fromTo(WhoRef.current, {
@@ -42,12 +43,7 @@ const AboutMe = () => {
     <>
     <section ref={sectionRef} className={styles['about_me']}>
       <div className={styles.apresentation}>
-        <h1 ref={WhoRef} className={styles.gsap_wia}>Who I' am ??</h1>
-
-        <h2 ref={softwareEngineerRef} id='gsap_se'>Software Engineer</h2>
-
-        <h2 ref={developerRef} id='gsap_fd'>Frontend Developer</h2> 
-
+        <h1 ref={WhoRef} className={styles.gsap_wia}>Sant v7</h1>
 
 
 
@@ -56,17 +52,52 @@ const AboutMe = () => {
             <source src={newPc} type='video/mp4'/>
           </video>
 
-           <video className={styles.robot} preload='auto' autoPlay muted loop playsInline >
+           {/* <video className={styles.robot} preload='auto' autoPlay muted loop playsInline >
             <source src={robot} type='video/mp4'/>
-          </video>      
+          </video>     */}
          
         </section>
 
-         <div className={styles.old_pc}>
-          <img src={oldPc} alt=" Old PC" />
-        </div> 
+          <img className={styles.old_pc} src={oldPc} alt=" Old PC" />
 
-              
+          <img className={styles.keyboard_pixel} src={teclado_pixel} alt="Teclado" />
+
+      </div>
+
+
+
+
+      <div className={styles.about_me_card}>
+        <header>
+          <h2>Vinícius</h2>
+          <h3>Level 18</h3>
+        </header>
+
+        <div className={styles.main_info}>
+          <img src="photo anime" alt="photo anime" />
+          <section className={styles.personal_data}>
+            <p className={styles.data_paragraph}>First Name: Vinícius</p>
+            <p className={styles.data_paragraph}>Last Name: Santos Oliveira</p>
+            <p className={styles.data_paragraph}>Age: 18</p>
+            <p className={styles.data_paragraph}>Profession: Software Engineer</p>
+            <p className={styles.data_paragraph}>Stack: Font-end</p>
+
+            <footer className={styles.info_card_footer}>
+              <p className={styles.data_paragraph}>Status: Active</p>
+              <button className={styles.more_info}>More</button>
+            </footer>
+          </section>
+        </div>
+
+        <div className={styles.grid_template}>
+          <div className="digital"></div>
+          <div className="city-bsb"></div>
+          <article className={styles.text_about_me}></article>
+        </div>
+
+        <div>
+          {/* tenho qeu colocar o video aqui, ( do pc e piano) */}
+        </div>
       </div>
       
     </section>
