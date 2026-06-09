fa70c5484409025fb0ac83bfbeff2d6aadb608ca
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-06-09 17:11:53 -0300

    Criando oestilo

---
+++ src/components/error/Error404.tsx
@@ -0,0 +1,23 @@
+import styles from './error404.module.css'
+import girl from '../../assets/img/png/girl.png'
+
+const Error404 = () => {
+  return (
+    <>
+    <section className={styles.error_page}>
+        <div className={styles.area_center}>
+
+            <span className={styles.left_four}>4</span>
+
+            <div className={styles.girl_area}>
+                <img src={girl} alt="girl error" />
+            </div>
+
+            <span className={styles.right_four}>4</span>
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default Error404
