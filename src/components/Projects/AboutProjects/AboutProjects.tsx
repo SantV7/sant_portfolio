69f84a66c15434615d51c6c162031af6b37552d2
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-06-14 04:32:14 -0300

    Creating focus list obj

--- src/components/Projects/AboutProjects/AboutProjects.tsx
+++ src/components/Projects/AboutProjects/AboutProjects.tsx
@@ -1,12 +1,46 @@
 import styles from './AboutProjects.module.css'
+
+interface ProjectFocus {
+    uiAndUx: string;
+    design: string;
+    structure: string;
+    development: string;
+    responsiveness: string;
+    tips : {
+        tratament?: string;
+        validation?: string;
+        pratics?: string;        
+    } []
+}    
+
 const AboutProjects = () => {
+
+
+    const focus: ProjectFocus = {
+        uiAndUx: 'UI/UX',
+        design: 'Design student',
+        structure: 'Clean Code and Semantics',
+        development: 'Front-End Development',
+        responsiveness:'Responsive Design',
+        tips : [
+            {tratament:'Tratamento de dados'},
+            {validation:'Validação'},
+            {pratics:'Boas práticas'}
+        ]
+    }
+
   return (
+    <>
+    <div>
+        <h1>Project Development Focus</h1>
+    </div>
     <section className={styles.about_projects}>
-        <div className={styles.g1}>UI/UX <br /> Designer</div>
-        <div className={styles.g2}></div>
-        <div className={styles.g3}></div>
-        <div className={styles.g4}></div>
-    </section>
+        <div className={styles.g1}>{focus.uiAndUx} <br></br> {focus.design}</div>
+        <div className={styles.g2}>{focus.structure}</div>
+        <div className={styles.g3}>{focus.development}</div>
+        <div className={styles.g4}>{focus.responsiveness}</div>
+    </section>    
+    </>
   )
 }
 
