04e293a4f26a7ee5518df9691db6afabbeee572d
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-06-14 04:07:25 -0300

    Criando o novo componente

---
+++ src/components/Projects/AboutProjects/AboutProjects.tsx
@@ -0,0 +1,13 @@
+import styles from './AboutProjects.module.css'
+const AboutProjects = () => {
+  return (
+    <section className={styles.about_projects}>
+        <div className={styles.g1}>UI/UX <br /> Designer</div>
+        <div className={styles.g2}></div>
+        <div className={styles.g3}></div>
+        <div className={styles.g4}></div>
+    </section>
+  )
+}
+
+export default AboutProjects
