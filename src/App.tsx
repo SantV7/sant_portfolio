567150f48954ca564337a951135e6237820e0930
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-06-16 01:54:50 -0300

    Deixando a rolagemmais suave

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
 
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 2.3,
+      duration: 4.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
     })
